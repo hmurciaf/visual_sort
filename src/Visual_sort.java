bc78bda750c1aeb07604cb8efe63c1e972af7d39
Hector Murcia Forero <hmurciaf@gmail.com>
2026-04-07 14:10:50 -0500

    Inicialización del proyecto visual_sort

---
+++ src/Visual_sort.java
@@ -0,0 +1,22 @@
+public class Visual_sort {
+    public static void main(String[] args) {
+        int vec[] = {7, 11, 22, -5, -9, 99, -1};
+        print(vec);
+    }
+    private static void print(int v[]) {
+        int s = v.length;
+        for (int i = 0; i < s; i++) {
+            if (i == 0) System.out.print("+");
+            System.out.print("--+");
+            if (i == s-1) System.out.print("\n");
+
+            if (i == 0) System.out.print("|");
+            System.out.printf("%2d|", v[i]);
+            if (i == s-1) System.out.print("\n");
+
+            if (i == 0) System.out.print("+");
+            System.out.print("--+");
+            if (i == s-1) System.out.print("\n");
+        }
+    }
+}
